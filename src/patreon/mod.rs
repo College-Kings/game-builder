33f2e942e208d685597da4169732460432e536f8
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-02-08 02:21:23 +0000

    Refactor patreon module and fix a bug in upload_game function

--- src/patreon/mod.rs
+++ src/patreon/mod.rs
@@ -1,3 +1,71 @@
 pub mod launcher_upload;
-pub mod patreon;
 pub mod upload_manifest;
+
+use std::{env, fs::File, path::PathBuf, thread, time::Duration};
+
+use reqwest::blocking::Client;
+use tokio::time;
+
+use crate::{build_game, BUNNY_PATH_ROOT, GAME_DIR, GAME_NAME, VERSION};
+
+pub async fn patreon() {
+    println!("Starting patreon process...");
+
+    let pc_build_thread = thread::spawn(|| build_game("pc", "zip"));
+    time::sleep(Duration::from_secs(30)).await;
+    let mac_build_thread = thread::spawn(|| build_game("mac", "zip"));
+
+    pc_build_thread.join().unwrap();
+    mac_build_thread.join().unwrap();
+
+    let pc_upload_thread = thread::spawn(|| upload_game(GAME_NAME, "pc"));
+    let mac_upload_thread = thread::spawn(|| upload_game(GAME_NAME, "mac"));
+
+    pc_upload_thread.join().unwrap();
+    mac_upload_thread.join().unwrap();
+}
+
+pub fn upload_game(game_name: &str, os: &str) {
+    println!("Uploading {} build...", os);
+
+    let game_name_without_spaces = game_name.replace(' ', "");
+
+    let bunny_root = BUNNY_PATH_ROOT.replace("{}", &game_name.replace(' ', "_").to_lowercase()); // BUG: BUNNY_ROOT has been changed
+    let url = format!(
+        "{}/{}-{}-{}.zip",
+        bunny_root, game_name_without_spaces, VERSION, os
+    );
+
+    let game_path_buf = PathBuf::from(GAME_DIR);
+
+    let file_path = format!(
+        r#"{}\{}-dists\{}-{}.zip"#,
+        game_path_buf.parent().unwrap().to_str().unwrap(),
+        game_name_without_spaces,
+        game_name_without_spaces,
+        os
+    );
+
+    let file = File::open(file_path).expect("File not found");
+    let file_size = file.metadata().unwrap().len();
+
+    let client = Client::builder().timeout(None).build().unwrap();
+
+    let response = client
+        .put(url)
+        .header(
+            "Accesskey",
+            env::var("BUNNY_ACCESS_KEY").expect("Missing Bunny Access Key"),
+        )
+        .header("Content-Length", file_size)
+        .body(file)
+        .send()
+        .unwrap();
+
+    // Check the response status code
+    if response.status().is_success() {
+        println!("File uploaded successfully!");
+    } else {
+        println!("Failed to upload the file. Status: {:?}", response.status());
+    }
+}
