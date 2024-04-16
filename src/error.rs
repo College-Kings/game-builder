574e784842265fda0c380a9ba51ef3a537caf248
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-04-16 16:03:58 +0100

    Refactor error handling

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,4 @@
-use std::{any::Any, path::PathBuf};
+use std::any::Any;
 
 pub type Result<T> = std::result::Result<T, Error>;
 
@@ -9,16 +9,33 @@ pub enum Error {
     EnvVar(std::env::VarError),
     SerdeJson(serde_json::Error),
     OsString(std::ffi::OsString),
+    Dotenvy(dotenvy::Error),
+    Bunny(bunny_cdn_wrapper::Error),
+    JoinError(tokio::task::JoinError),
     Thread(Box<dyn Any + Send>),
     GameNotFound,
-    InvalidPath(PathBuf),
+    InvalidPath(std::path::PathBuf),
+    VersionNotFound,
 }
 
-// impl fmt::Display for Error {
-//     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-
-//     }
-// }
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Error::Io(error) => write!(f, "IO error: {}", error),
+            Error::Reqwest(error) => write!(f, "Reqwest error: {}", error),
+            Error::EnvVar(error) => write!(f, "Environment variable error: {}", error),
+            Error::SerdeJson(error) => write!(f, "Serde JSON error: {}", error),
+            Error::OsString(error) => write!(f, "OsString error: {:?}", error),
+            Error::Dotenvy(error) => write!(f, "Dotenvy error: {}", error),
+            Error::Bunny(error) => write!(f, "Bunny error: {}", error),
+            Error::JoinError(error) => write!(f, "Join error: {}", error),
+            Error::Thread(_) => write!(f, "Thread error"),
+            Error::GameNotFound => write!(f, "Game not found"),
+            Error::InvalidPath(path) => write!(f, "Invalid path: {:?}", path),
+            Error::VersionNotFound => write!(f, "Version not found"),
+        }
+    }
+}
 
 impl From<std::io::Error> for Error {
     fn from(error: std::io::Error) -> Self {
@@ -47,5 +64,23 @@ impl From<serde_json::Error> for Error {
 impl From<std::ffi::OsString> for Error {
     fn from(error: std::ffi::OsString) -> Self {
         Error::OsString(error)
+    }
+}
+
+impl From<dotenvy::Error> for Error {
+    fn from(error: dotenvy::Error) -> Self {
+        Error::Dotenvy(error)
+    }
+}
+
+impl From<bunny_cdn_wrapper::Error> for Error {
+    fn from(error: bunny_cdn_wrapper::Error) -> Self {
+        Error::Bunny(error)
+    }
+}
+
+impl From<tokio::task::JoinError> for Error {
+    fn from(error: tokio::task::JoinError) -> Self {
+        Error::JoinError(error)
     }
 }
