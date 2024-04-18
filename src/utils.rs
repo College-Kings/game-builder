a5f952c8639880380f9b9d67f063178ad75565f9
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-04-18 20:17:46 +0100

    Add tokio_flatten function to handle async task results in utils.rs

---
+++ src/utils.rs
@@ -0,0 +1,11 @@
+use tokio::task::JoinHandle;
+
+use crate::Error;
+
+pub async fn tokio_flatten<T>(handle: JoinHandle<Result<T, Error>>) -> Result<T, Error> {
+    match handle.await {
+        Ok(Ok(result)) => Ok(result),
+        Ok(Err(err)) => Err(err),
+        Err(err) => Err(err.into()),
+    }
+}
